7862f166a2d82edce57ad6e95edf184c5b5b341f
Prithvi Singh <prithvi.inbox@outlook.com>
2026-06-18 03:42:09 +0530

    nest fix

--- nvim/nvim/lua/plugins/vim-highlighter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "azabiong/vim-highlighter",
-    init = function()
-        vim.g.HiSet = "<leader>mh"
-        vim.g.HiErase = "<leader>mr"
-        vim.g.HiClear = "<leader>mc"
-    end,
-}
